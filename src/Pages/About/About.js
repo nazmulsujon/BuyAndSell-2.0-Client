384e173c4cbdc04d00ea29ce77c9be0aa6abaca4
Nazmul Sujon <sujon686500@gmail.com>
2022-11-28 15:08:20 +0600

    private route , authProvider & Spinner added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Spinner from "../Shared/Spinner/Spinner";
 
 const About = () => {
   return (
     <div>
       <h2>this is about page</h2>
+      <Spinner></Spinner>
     </div>
   );
 };
