88db23e729cc8cd5c7db419fb6237fc25d14e402
Nazmul Sujon <sujon686500@gmail.com>
2022-11-28 16:57:45 +0600

    log out method added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,7 @@ import slider3 from "../../../assets/banner/slider-3.webp";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full h-[70vh]">
+    <div className="carousel w-full h-[80vh]">
       <div id="slide1" className="carousel-item relative w-full">
         <img src={slider1} alt="slider" className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
+import { AuthContext } from "../../../Contexts/AuthProvider/AuthProvider";
 
 const Header = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then((res) => console.log("log out successfully"))
+      .catch((err) => console.error(err));
+  };
+
   const menuItems = (
     <React.Fragment>
       <li className="mr-1 my-1 font-semibold">
@@ -34,30 +43,6 @@ const Header = () => {
   );
 
   return (
-    // <section className="navbar container mx-auto">
-    //   <div className="navbar-start w-1/2">
-    //     <div className="dropdown">
-    //       <label tabIndex={0} className="btn btn-ghost lg:hidden">
-    //         <svg
-    //           xmlns="http://www.w3.org/2000/svg"
-    //           className="h-5 w-5"
-    //           fill="none"
-    //           viewBox="0 0 24 24"
-    //           stroke="currentColor"
-    //         >
-    //           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-    //         </svg>
-    //       </label>
-    //       <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-    //         {menuItems}
-    //       </ul>
-    //     </div>
-    //     <Link className="normal-case text-xl font-bold italic text-info shadow-sm rounded-lg p-2">BUY & SELL</Link>
-    //   </div>
-    //   <div className="navbar-center hidden lg:flex navbar-end w-1/2">
-    //     <ul className="menu menu-horizontal p-0">{menuItems}</ul>
-    //   </div>
-    // </section>
     <section className="bg-neutral">
       <div className="navbar md:px-10">
         <div className="navbar-start w-11/12">
@@ -90,7 +75,9 @@ const Header = () => {
           <ul className="menu menu-horizontal p-0">{menuItems}</ul>
         </div>
         <div className="navbar-end">
-          <button className="btn btn-outline rounded-lg">Logout</button>
+          <button onClick={handleLogOut} className="btn btn-outline rounded-lg">
+            Logout
+          </button>
         </div>
       </div>
     </section>
